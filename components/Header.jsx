b512cfe9b63af03f6c4fe55a69cc3aff1d1008f5
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-07-15 21:10:08 +0530

    top nav completed

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,15 +13,15 @@ function Header() {
       <div className="header__container">
 
         {/* Top Nav */}
-        <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2 px-3">
           <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
             <Image
               src="https://links.papareact.com/f90"
               width={150}
               height={40}
               alt="Logo"
-              objectFit="contain"
-              className="cursor-pointer"
+              // objectFit="contain"
+              className="cursor-pointer object-contain mr-1"
             />
           </div>
 
@@ -30,6 +30,29 @@ function Header() {
               <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
               <SearchIcon className="h-12 p-4" />
               
+          </div>
+
+          {/* Right Header */}
+          <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+              <div className="link">
+                  <p>Hello, Manas</p>
+                  <p className="font-extrabold md:text-sm">Account & Lists</p>
+              </div>
+
+              <div className="link">
+                <p>
+                  Return
+                </p>
+                <p className="font-extrabold md:text-sm">
+                   and Orders
+                </p>
+              </div>
+
+              <div className="relative link flex items-center">
+                <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+              <ShoppingCartIcon className="h-10" />
+              <p className="hidden md:inline font-extrabold md:text-sm mt-2">Cart</p>
+              </div>
           </div>
 
         </div>

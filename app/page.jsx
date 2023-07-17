259121b027dbc0144bfa56b711e68331bb701368
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-07-17 15:11:39 +0530

    Setting up SSR in App Router & Data Fetching

--- app/page.jsx
+++ app/page.jsx
@@ -10,8 +10,6 @@ export default async function Home() {
 
   return (
       <div className="home bg-gray-100">
-        {products[0].id}
-        {products[0].title}
         <Header />
 
         <main className="max-w-screen-2xl mx-auto">
@@ -21,50 +19,12 @@ export default async function Home() {
 
 
           {/* ProductFeed Start */}
-          {/* <ProductFeed products={products} sampleprop = "Hello Guys" /> */}
+          <ProductFeed products={products} />
           {/* ProductFeed End */}
-          {products.map(product => {
-            return (
-              <p key={product.id}>{product.id} {product.title}</p>
-            )
-          })}
         </main>
       </div>
   )
 }
 
-// // Thought Note: Understanding Server Side Data Fetching in Page Router
-// Below Methods are for Next Page Router
 
-// First Way
-// export async function getServerSideProps() {
-//   const products = await fetchData();
-
-//   return {props: products,
-//   }
-// }
-
-
-
-// Second Way
-// export async function getServerSideProps(context) {
-//   try {
-//     const response = await fetch('https://fakestoreapi.com/products');
-//   const data = await response.json();
-
-//   return {
-//     props: {
-//       data,
-//     }
-//   }
-//   } catch (error) {
-//     console.error(error);
-//     // Handle error or return an error page
-//     return {
-//       props: {
-//         data: [],
-//       },
-//     };
-//   }
-// }
 
--- utils/api.js
+++
@@ -1,9 +0,0 @@
-// Thought Note: Understanding Server Side Data Fetching in Page Router
-
-
-
-export async function fetchData() {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
-    return data;
-  }

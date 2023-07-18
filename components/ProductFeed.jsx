4dd84dd9ce74ab80fd6180b67d7952ef2ef13ae8
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-07-18 16:23:47 +0530

    Ad section added to Feed

--- components/ProductFeed.jsx
+++ components/ProductFeed.jsx
@@ -1,9 +1,48 @@
+import Image from "next/image"
 import Product from "./Product"
 
 function ProductFeed({products}) {
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-        {products.map(({id, title, price, description, category, image, rating}) => (
+        {products.slice(0,4)
+        .map(({id, title, price, description, category, image, rating}) => (
+          <Product
+                key={id}
+                id={id}
+                title={title}
+                price={price}
+                description={description}
+                category={category}
+                image={image}
+                rating={rating}
+           />
+        ))}
+
+        <Image className="w-full md:col-span-full" 
+              src="https://links.papareact.com/dyz" 
+              alt="ad image"
+              width={1000}
+              height={200}
+               />
+        
+        <div className="md:col-span-2">
+        {products.slice(4,5)
+        .map(({id, title, price, description, category, image, rating}) => (
+          <Product
+                key={id}
+                id={id}
+                title={title}
+                price={price}
+                description={description}
+                category={category}
+                image={image}
+                rating={rating}
+           />
+        ))}
+        </div>
+
+        {products.slice(5, products.length)
+        .map(({id, title, price, description, category, image, rating}) => (
           <Product
                 key={id}
                 id={id}

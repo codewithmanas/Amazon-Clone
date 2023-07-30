9cc2c592acd229e58c036248355066a30911c25f
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-07-30 17:24:33 +0530

    installed redux-toolkit for future integration

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,11 +1,31 @@
 import Header from '@components/Header'
+import Image from 'next/image'
 import React from 'react'
 
 function page() {
   return (
     <div className="bg-gray-100">
         <Header />
-        <h1 className='text-3xl'>This is Checkout Page</h1>
+        <main className="lg:flex max-w-screen-2xl mx-auto">
+            {/* Left */}
+            <div className="flex-grow m-5 shadow-sm">
+              <Image
+                src="https://links.papareact.com/ikj"
+                width={1020}
+                height={250}
+                alt="checkout image"
+                className="object-contain"
+              />
+
+              <div className="flex flex-col p-5 space-y-10 bg-white">
+                <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+              </div>
+
+            </div>
+
+
+            {/* Right */}
+        </main>
     </div>
   )
 }
